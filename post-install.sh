73356aa4378d169e14f2b763f703f58830e535bc
DamienPlantin <77361513+DamienPlantin@users.noreply.github.com>
2021-02-10 16:39:06 +0100

    Update post-install.sh

--- post-install.sh
+++ post-install.sh
@@ -8,8 +8,7 @@ sudo apt-get update  >> log.txt
 #La commande sudo permet d'exécuter la commande en super-utilisateur
 #La commande apt-get permet d'effectuer l'installation et la désinstallation de paquets en provenance d'un dépôt APT.
 #Update permet de mettre à jour la commande placer juste avant
-clear
-#Cette commande permet d'effacer l'écran
+
 echo "Installing base packages"  >> log.txt
 
 sudo apt-get install --yes git git-extras build-essential python3-pip htop glances  >> log.txt
@@ -19,14 +18,8 @@ echo "Installing discord"  >> log.txt
 
 sudo snap install discord  >> log.txt
 #snap install permet d'installer l'application via le store d'application ubuntu
-clear
-
 echo "Installing Visual Code Studio"  >> log.txt
 
 sudo snap install code --classic  >> log.txt
 
-clear
-
 echo "Installing finish"  >> log.txt
-
-clear
